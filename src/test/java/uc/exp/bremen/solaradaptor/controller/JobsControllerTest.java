cd40d73595c33bc9072426e6ae2c2ca5fdfef8e6
assetozen <assetozen@yahoo.com>
2020-01-28 16:46:53 +0100

    Test using mockito has been started

--- src/test/java/uc/exp/bremen/solaradaptor/controller/JobsControllerTest.java
+++ src/test/java/uc/exp/bremen/solaradaptor/controller/JobsControllerTest.java
@@ -1,8 +1,46 @@
 package uc.exp.bremen.solaradaptor.controller;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.runners.MockitoJUnitRunner;
+import uc.exp.bremen.solaradaptor.service.InitSolarService;
+import uc.exp.bremen.solaradaptor.service.SetUpConWithSolarAndReadContentService;
+
+import java.io.BufferedReader;
+import java.net.URL;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class JobsControllerTest {
+    @Mock
+    private InitSolarService initSolarService;
+
+    @Mock
+    private SetUpConWithSolarAndReadContentService setUpConWithSolarAndReadContentService;
+
+    @InjectMocks
+    private JobsController jobsController;
+
+    @BeforeAll
+    public void initializeMockito(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    public void getJobsFromSolarTest(){
+        String sSearch = "java";
+        String sSort = "1";
+        String sFilter = "1";
+
+        URL url = initSolarService.initSolarUrl(sSearch,sSort,sFilter);
+        BufferedReader br = setUpConWithSolarAndReadContentService.doSetUpConnectionWithSolarAndReadData(url);
+
+
+
+    }
+
 
 
 }
